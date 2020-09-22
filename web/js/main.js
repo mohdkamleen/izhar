2810901ba1150dfd19e9f8e152e38e288486639f
mohdkamleen <kamleenmohd@gmail.com>
2020-09-22 19:07:36 +0530

    gfdgdsg

--- web/js/main.js
+++ web/js/main.js
@@ -1,17 +1,35 @@
 let preloader = document.querySelector("div.preloader"); 
 let preloader_counter = document.querySelector("div.preloader span b"); 
+let nav_hamberger_background = document.querySelector("section .body_content_header .nav_hamburger_background"); 
 
 window.onload = () => {
     preloader.classList.add("active"); 
 }  
+
+window.onscroll = () => {
+    let windowPageYOffset = window.pageYOffset;
+    if(windowPageYOffset > 400){
+        nav_hamberger_background.classList.add("show");
+    } else {
+        nav_hamberger_background.classList.remove("show");
+    }
+}
  
-setInterval(function(){
-    var count_number = 0; 
+
+// splash screen counter 
+var count_number = 0; 
+let preloader_counter_function = setInterval(function(){
     count_number++;
     preloader_counter.innerHTML = count_number; 
-},1000) 
+    
+    if (count_number == 100){
+        clearInterval(preloader_counter_function);
+    }
+},25) 
 
 
 
-// let preloader_counter_function;  
+
+
+
  
